2b995c2cde7d4887acab885122b76e2425e3095a
bradgayheart <b.gayheart3@gmail.com>
2020-11-05 15:54:44 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+$('.navItem').css('color', 'white');
+$('#nav').css('background-color', '#145');
+$('li:even').css('background-color', '#ccc');
+$('li:odd').css('background-color', '#aaa');
+var inputs = $(':input');
+$(inputs[0]).val("Brad");
